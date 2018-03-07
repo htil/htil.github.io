36acd749b9e491f1191d1ceaffef815361ab89de
pwstegman <pwstegman@gmail.com>
2018-03-07 15:36:26 -0600

    Work in progress index page

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var pug = require('gulp-pug');
 
 gulp.task('html', function () {
-    return gulp.src('templates/index.pug')
+    return gulp.src('templates/main.pug')
         .pipe(pug())
         .pipe(gulp.dest('./'))
 });
