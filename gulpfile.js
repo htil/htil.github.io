1f6156c1eabb6a620adb15b7130b674ef2d72ca8
Nicholas Cioli <nicholascioli@gmail.com>
2018-03-07 22:55:50 -0600

    switch css to sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,22 @@
 var gulp = require('gulp');
 var pug = require('gulp-pug');
+var sass = require('gulp-sass');
+var moment = require('moment');
+
+gulp.task('sass', function () {
+    return gulp.src('./sass/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./css'));
+});
 
 gulp.task('html', function () {
     return gulp.src('templates/main.pug')
-        .pipe(pug())
+        .pipe(pug({
+            locals: {
+                date: moment().format('HH:MM MM/DD/YYYY')
+            }
+        }))
         .pipe(gulp.dest('./'))
 });
 
-gulp.task('default', ['html']);
+gulp.task('default', ['sass', 'html']);
